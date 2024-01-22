84cc66bc20bdef057e00b6ca97b7b5e275e1bfed
IlhamArbani <ilham.na.ar@gmail.com>
2024-01-22 13:40:50 +0700

    setup alias path

---
+++ src/pages/loginPage/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Go Finance</h1>
+      <div>
+        <h2>Hello Again</h2>
+        <h3>Welcome back</h3>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
